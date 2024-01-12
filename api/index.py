4fcbd6d6a98fd8a07a726af746c05ee02796a5e1
Tefff <oscar.anh009@gmail.com>
2024-01-12 10:43:56 +0700

    fix

--- api/index.py
+++ api/index.py
@@ -1,14 +1,10 @@
 import os
-
-import replicate
 from flask import Flask, request, jsonify
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
-os.environ.get("REPLICATE_API_TOKEN")
-
 
 @app.route('/api/recommend', methods=['POST'])
 def recommend_products():
@@ -59,16 +55,3 @@ def recommend_products():
 def index():
     
     return "This is an alt tag generator!"
-
-@app.route('/generate')
-def home():
-  # Get imageUrl query param
-  args = request.args
-  imageUrl = args.to_dict().get('imageUrl')
-
-  # Run ML Model with imageUrl
-  model = replicate.models.get("salesforce/blip")
-  version = model.versions.get("2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746")
-
-  # Get the alt text result and return it
-  return version.predict(image=imageUrl)
